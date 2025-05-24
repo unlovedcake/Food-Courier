c10e35e3bba741ffe19c73378746cdefa16bb1f2
Mon143 <mon.firstbostonsoftware@gmail.com>
2025-05-24 11:27:33 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:food_courier/firebase_options.dart';
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+Future<Map<String, dynamic>> loadConfig() async {
+  String environment =
+      const String.fromEnvironment('env', defaultValue: 'development');
+
+  final String configString =
+      await rootBundle.loadString('assets/env/$environment.json');
+  return json.decode(configString);
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final Map<String, dynamic> json = await loadConfig();
+
+  runApp(
+    GetMaterialApp(
+      title: json['flavor'] ?? '',
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+}
